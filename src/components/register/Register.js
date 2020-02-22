6d34b80d3ac943e4a81e02ef5892013975913c3d
Italo Franco <italo.ravina@gmail.com>
2020-02-22 19:56:30 +0100

    first version register form

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import  { Container, Button, Form } from 'react-bootstrap' ;
 
 export default class Register extends Component{
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log("voy a ads");
+    }
+
+    
     render(){
         return(
-            <div>
-                <h2>Registro</h2>
-            </div>  
+            <Container>
+                <Form onSubmit={this.handleSubmit}>
+                    <div>
+                        <h2>Registro</h2>
+                    </div>  
+                    <Form.Group>
+                        <Form.Label>Usuario</Form.Label>
+                        <Form.Control type="text" placeholder="usuario" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Contraseña</Form.Label>
+                        <Form.Control type="password" placeholder="contraseña" />
+                    </Form.Group>
+                    <Button variant="secondary" type="submit">
+                        Registrarse
+                    </Button>
+                </Form>
+            </Container>
         );
     }
 }
