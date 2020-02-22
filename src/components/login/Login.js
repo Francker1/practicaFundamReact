6eaf170f2cc16ccf335e407e18d8550f772193a3
Italo Franco <italo.ravina@gmail.com>
2020-02-22 13:27:24 +0100

    add login form first version

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 export default class Login extends Component{
     render(){
         return(
+            <>
             <div>
                 <h2>Login</h2>
             </div>  
+            <div>
+            <form>
+                <label>
+                    Name:
+                    <input type="text" name="name" />
+                </label>
+                <label>
+                    Pass:
+                    <input type="password" name="name" />
+                </label>
+                <input type="submit" value="Submit" />
+                </form>
+            </div>
+            </>
         );
     }
 }
