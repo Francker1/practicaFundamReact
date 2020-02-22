516374a0bbcf5442b52a37364437e433614cc487
Italo Franco <italo.ravina@gmail.com>
2020-02-22 21:22:14 +0100

    add axios to post register

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import  { Container, Button, Form } from 'react-bootstrap' ;
 
 export default class Register extends Component{
 
-    handleSubmit = e => {
-        e.preventDefault();
-        console.log("voy a ads");
+    constructor(props){
+        super(props);
+        this.state = {
+            username: "", 
+            password: ""   
+        }
+    }
+    
+    handleUsernameChange = ev => {
+        this.setState({
+            username: ev.target.value
+        })
     }
 
+    handlePassChange = ev =>{
+        this.setState({
+            password: ev.target.value
+        })
+    }
     
+    handleSubmit = ev => {
+        ev.preventDefault();
+        const {username, password} = this.state;
+
+        axios.post("http://34.89.93.186:8080/apiv1/register", {  
+            username: username,
+            password: password 
+        })
+        .then(res => {
+            console.log(res);
+            console.log(res.data);
+        })
+
+    }
+
+
     render(){
+        const { username, password } = this.state;
         return(
             <Container>
                 <Form onSubmit={this.handleSubmit}>
@@ -18,11 +50,23 @@ export default class Register extends Component{
                     </div>  
                     <Form.Group>
                         <Form.Label>Usuario</Form.Label>
-                        <Form.Control type="text" placeholder="usuario" />
+                        <Form.Control 
+                            required
+                            type="text" 
+                            placeholder="usuario" 
+                            value={username} 
+                            onChange={this.handleUsernameChange}   
+                        />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Contraseña</Form.Label>
-                        <Form.Control type="password" placeholder="contraseña" />
+                        <Form.Control 
+                            required
+                            type="password" 
+                            placeholder="contraseña" 
+                            value={password}
+                            onChange={this.handlePassChange}
+                        />
                     </Form.Group>
                     <Button variant="secondary" type="submit">
                         Registrarse
